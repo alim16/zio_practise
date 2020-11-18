d128edc87ff4952bf04b4e9c49f4e41c514e81c6
Mahamed Ali <mahamed.ali@concentra.co.uk>
2020-11-18 10:29:13 +0000

    attempt at rewriting server file as a module

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,23 +3,20 @@ import zio._, zio.console._
 
 import mo.example.fileStuff.FileIO 
 import mo.example.streaming.StreamingStuff
-import mo.example.http.Http4Server.Http4Server
-import mo.example.http.Http4Server 
+import mo.example.http.server.HttpServer
 import org.http4s.server.Server
 import zio.stream.ZStream
 import zio.duration._
 import zio.clock.Clock
 
-
 object Main extends zio.App {
  
-
   val appLogic = 
   for {
     _ <- putStrLn("hello")
     n <- StreamingStuff.numberStream
     _ <- putStrLn(s"the numbers are: ${n.toString}")
-    serverFiber <- ZIO.never.fork
+    serverFiber <- HttpServer.startHttpServer.fork
     _ <- putStrLn("server is running on localhost:8080")
     //the next line kills the main fiber after 30s which kills the child server fiber
     _ <- ZIO.sleep(30.seconds) *> putStrLn("30 seconds over: shutting down server...")
@@ -28,20 +25,7 @@ object Main extends zio.App {
 
   def run(args: List[String]) =  appLogic.provideLayer(prepareEnvironment).exitCode
 
- val httpServerLayer: ZLayer[ZEnv, Throwable, Http4Server] = Http4Server.createHttp4sLayer
-
-  private val prepareEnvironment = Clock.live ++ Console.live ++ StreamingStuff.live ++ httpServerLayer 
+  private val prepareEnvironment = Clock.live ++ Console.live ++ StreamingStuff.live ++ HttpServer.live
   
-
 }
 
-/* the original way to run server implementation (from article)
-
-  val appLogic:ZIO[Has[Server] with Console, Nothing, Nothing] = ZIO.never
-
-  def run(args: List[String]) =  appLogic.provideLayer(prepareEnvironment).exitCode
-
-  val httpServerLayer: ZLayer[ZEnv, Throwable, Http4Server] = Http4Server.createHttp4sLayer
-
- // private val prepareEnvironment = httpServerLayer ++ Console.live //++ FileIO.live 
-*/
--- src/main/scala/http-service/HttpServer.scala
+++ src/main/scala/http-service/HttpServer.scala
@@ -4,23 +4,70 @@ import zio._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 
-import org.http4s.server.Server
+import org.http4s.server._
+//import org.http4s.server.blaze._
 import org.http4s.server.blaze.BlazeServerBuilder
 
 //https://github.com/kovacshuni/zio-http4s-zlayer-example
-object Http4Server {
-    type Http4Server = Has[Server]
+package object server {
+  type HttpServer = Has[HttpServer.Service]
 
-    def createHttp4Server: ZManaged[ZEnv,Throwable, Server] =
-        ZManaged.runtime[ZEnv].flatMap { implicit runtime: Runtime[ZEnv] =>
-            BlazeServerBuilder[Task](runtime.platform.executor.asEC)
-                .bindHttp(8080,"localhost")
-                .withHttpApp(Routes.helloWorldsService)
-                .resource
-                .toManagedZIO
-        }
-    
-    def createHttp4sLayer: ZLayer[ZEnv, Throwable, Http4Server] =
-        ZLayer.fromManaged(createHttp4Server)
+  object HttpServer {
+    trait Service {
+      def startHttpServer():ZManaged[HttpServer, Nothing, Server]
+    }
+
+    private def startHttp4Server: ZManaged[Any, Throwable, Server] =
+      ZIO.runtime[Any].toManaged_.flatMap { implicit runtime =>
+        BlazeServerBuilder[Task](runtime.platform.executor.asEC)
+          .bindHttp(8080, "localhost")
+          .withHttpApp(Routes.helloWorldsService)
+          .resource
+          .toManagedZIO
+      }
+  val live : ZLayer[Any,Nothing,HttpServer]  = ZLayer.succeed(
+      new Service {
+        override def startHttpServer():ZManaged[Any, Nothing, Server] = startHttp4Server.orDie
+      }
+    )
+
+    def startHttpServer:  URIO[HttpServer, Server] = ZIO.accessM(_.get.startHttpServer.useForever)
+  }
+
 }
 
+
+
+
+
+///////////////////////////////////////////
+
+// package object server {
+//   type HttpServer = Has[HttpServer.Service]
+
+//   object HttpServer {
+//     trait Service {
+//       def createHttpLayer():ZIO[ZEnv, Throwable, Server]
+//     }
+
+//     private def createHttp4Server: ZManaged[ZEnv, Throwable, Server] =
+//       ZManaged.runtime[ZEnv].flatMap { implicit runtime: Runtime[ZEnv] =>
+//         BlazeServerBuilder[Task](runtime.platform.executor.asEC)
+//           .bindHttp(8080, "localhost")
+//           .withHttpApp(Routes.helloWorldsService)
+//           .resource
+//           .toManagedZIO
+//       }
+
+//     val live : ZLayer[Any,Nothing,HttpServer]  = ZLayer.succeed(
+//       new Service {
+//         override def createHttpLayer() = createHttp4Server //ZLayer.fromManaged(createHttp4Server)
+//       }
+//     )
+
+//     def createHttpLayer:  URIO[HttpServer, Server] = ZIO.accessM(_.get.createHttpLayer)
+//   }
+
+//   // def createHttp4sLayer: ZLayer[ZEnv, Throwable, Http4Server] =
+//   //     ZLayer.fromManaged(createHttp4Server)
+// }
